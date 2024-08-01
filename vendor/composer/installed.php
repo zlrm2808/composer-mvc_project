77d35ba21b3ee997c22c3fb3106b985c5aa4f7e4
zlrm2808 <zlrm2808@gmail.com>
2024-08-01 12:17:56 -0400

    cambios

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zrodriguez/composer-mvc_project',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0936134e02ca5d8b43460f0ac82c199188ef709e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zrodriguez/composer-mvc_project' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0936134e02ca5d8b43460f0ac82c199188ef709e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
